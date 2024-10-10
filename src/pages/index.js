28c61be450e4ce3020755117f218e1961f3e478c
Jakkapan Pakeerat <jakkapanpakeerat@Jakkapans-MacBook-Air.local>
2024-10-10 20:39:10 +0700

    check reason login fail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import NumberGrid from "@/components/NumberGrid/NumberGrid";
+
+import liff from "@line/liff";
+
 const Index = () => {
+  const fetchUserProfile = async () => {
+    try {
+      const profile = await liff.getProfile();
+      console.log("User Profile:", profile);
+      // You can now use profile.displayName, profile.pictureUrl, etc.
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchUserProfile();
+  }, []);
   return (
     <div>
       <Typography alignContent={"center"} variant="h6" gutterBottom>
